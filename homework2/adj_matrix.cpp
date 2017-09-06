25024873b21d6b4bfbb9d1b55420f8d2455bdfbd
eirkrw <ering-walters@csumb.edu>
2017-09-06 05:33:40 +0000

    first commit for homework2

---
+++ homework2/adj_matrix.cpp
@@ -0,0 +1,28 @@
+/*
+* Title: adj_matrix.cpp
+* Abstract: This program takes a file path and reads the contents of a described graph, then outputs the properties of the graph.
+* Author: Erik Ring-Walters
+* ID: 7574
+* Date: 09/05/2017
+*/
+
+using namespace std;
+#include <iostream>
+#include <cmath>
+#include <fstream>
+
+void main()
+{
+    ifstream infile;
+    string filename;
+    cout <<"Enter input file name: ";
+    cin >> filename;
+    if(filename == "")
+    {
+        cout << "Invalid file name";
+        return;
+    }
+    
+    return;
+}
+
